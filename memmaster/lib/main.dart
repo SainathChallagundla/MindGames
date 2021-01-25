768c7bb1511ca3684a0f1e5c07d3dc2316252a49
Sainath <Sainath@Macs-MacBook-Pro.local>
2021-01-26 02:31:35 +0530

    BackGround Gradient Added

--- memmaster/lib/main.dart
+++ memmaster/lib/main.dart
@@ -1,19 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:memmaster/HomePage.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'HomePage.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(new MaterialApp(
+    theme: new ThemeData(
+        primaryColor: Colors.cyan,
+        primaryColorDark: Colors.tealAccent[900],
+        accentColor: Colors.tealAccent),
+    home: new MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return new SplashScreen(
+      title: new Text(
+        'Memory Master',
+        style: TextStyle(
+            color: Colors.cyan,
+            decorationStyle: TextDecorationStyle.wavy,
+            //fontFamily: 'Pacifico',
+            fontWeight: FontWeight.bold,
+            fontSize: 50.0),
       ),
-      home: HomePage(),
+      seconds: 1,
+      loadingText: Text(
+        'Welcome To Meemory Games ',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),
+      ),
+      useLoader: true,
+      navigateAfterSeconds: new HomePage(),
+      //image: new Image.asset('assets/Images/GimpStStrokePink.png'),
+      // imageBackground:
+      //     new AssetImage('assets/display images/Eiffel Tower.jpeg'),
+      gradientBackground: LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        colors: [Colors.white, Colors.cyan[100], Colors.cyan[100]],
+      ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:memmaster/HomePage.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: HomePage(),
+//     );
+//   }
+// }
