93aa8bc735529f8cf47c117718edd42a7cf53ed8
Sainath <Sainath@Macs-MacBook-Pro.local>
2021-02-03 01:29:39 +0530

    SharedPreferances added

--- memmaster/lib/Settings.dart
+++ memmaster/lib/Settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_number_picker/flutter_number_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
+import 'LocalStore.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -8,12 +10,49 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  int _noOfDigits;
+  int _noOfRandNums;
+  int _displaySeconds;
+  int _enterTime;
+
+  @override
+  void initState() {
+    super.initState();
+    onLoad();
+  }
+
+  onLoad() async {
+    print("<=============== Settings  OnLoad Called================>");
+    int noOfDigits = await getnoOfDigits();
+    print("=========>Settings<==========noOfDigits$noOfDigits");
+
+    setState(() {
+      _noOfDigits = noOfDigits;
+    });
+
+    int noOfRandNums = await getnoOfRandNums();
+    print("=========>Settings<==========noOfRandNums$noOfRandNums");
+
+    setState(() {
+      _noOfRandNums = noOfRandNums;
+    });
+
+    int displaySeconds = await getnoOfDigits();
+    print("=========>Settings<==========displaySeconds$displaySeconds");
+    setState(() {
+      _displaySeconds = displaySeconds;
+    });
+
+    int enterTime = await getenterTime();
+    print("=========>Settings<==========enterTime$enterTime");
+
+    setState(() {
+      _enterTime = enterTime;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    int digitLength = 2;
-    int randNumLength = 5;
-    int displaySeconds = 7;
-    int enterTime = 10;
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
@@ -28,6 +67,13 @@ class _SettingsState extends State<Settings> {
               "Select How Many Digits Number Do You Want",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
             ),
+            // NumberInputPrefabbed.roundedButtons(
+            //   min: 3,
+            //   max: 6,
+            //   controller: TextEditingController(),
+            //   incDecBgColor: Colors.amber,
+            //   buttonArrangement: ButtonArrangement.incRightDecLeft,
+            // ),
             Container(
               child: CustomNumberPicker(
                 valueTextStyle: TextStyle(
@@ -42,12 +88,12 @@ class _SettingsState extends State<Settings> {
                   Icons.remove,
                   size: 30,
                 ),
-                initialValue: digitLength,
-                maxValue: 5,
-                minValue: 2,
+                initialValue: _noOfDigits,
+                maxValue: 3,
+                minValue: 1,
                 step: 1,
-                onValue: (value) {
-                  print(value.toString());
+                onValue: (value) async {
+                  await setnoOfDigits(value);
                 },
               ),
             ),
@@ -69,12 +115,12 @@ class _SettingsState extends State<Settings> {
                   Icons.remove,
                   size: 30,
                 ),
-                initialValue: randNumLength,
-                maxValue: 30,
-                minValue: 3,
+                initialValue: _noOfRandNums,
+                maxValue: 5,
+                minValue: 2,
                 step: 1,
-                onValue: (value) {
-                  print(value.toString());
+                onValue: (value) async {
+                  await setnoOfRandNums(value);
                 },
               ),
             ),
@@ -96,12 +142,12 @@ class _SettingsState extends State<Settings> {
                   Icons.remove,
                   size: 30,
                 ),
-                initialValue: displaySeconds,
-                maxValue: 30,
+                initialValue: _displaySeconds,
+                maxValue: 15,
                 minValue: 3,
                 step: 1,
-                onValue: (value) {
-                  print(value.toString());
+                onValue: (value) async {
+                  await setdisplaySeconds(value);
                 },
               ),
             ),
@@ -123,15 +169,23 @@ class _SettingsState extends State<Settings> {
                   Icons.remove,
                   size: 30,
                 ),
-                initialValue: enterTime,
-                maxValue: 10,
-                minValue: 2,
+                initialValue: _enterTime,
+                maxValue: 30,
+                minValue: 7,
                 step: 1,
-                onValue: (value) {
-                  print(value.toString());
+                onValue: (value) async {
+                  await setenterTime(value);
                 },
               ),
             ),
+            IconButton(
+                icon: Icon(Icons.reset_tv),
+                onPressed: () async {
+                  await removedisplaySeconds();
+                  await removenoOfDigits();
+                  await removeenterTime();
+                  await removenoOfRandNums();
+                })
           ],
         ),
       ),
@@ -140,7 +194,3 @@ class _SettingsState extends State<Settings> {
 }
 
 // Take 4 keys ..
-setValue(String key, int value) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  await prefs.setInt(key, value);
-}
